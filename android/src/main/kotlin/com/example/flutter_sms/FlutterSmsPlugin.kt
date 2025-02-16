72cf462e132aa4e2efdeed11ecc4342c580cdd7d
Vetri Balaji <vetri@extocard.com>
2025-02-16 11:28:53 -0800

    Remove Registrar

--- android/src/main/kotlin/com/example/flutter_sms/FlutterSmsPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_sms/FlutterSmsPlugin.kt
@@ -18,7 +18,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 
 class FlutterSmsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
@@ -57,17 +56,6 @@ class FlutterSmsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
 
   private fun teardown() {
     mChannel.setMethodCallHandler(null)
-  }
-
-  // V1 embedding entry point. This is deprecated and will be removed in a future Flutter
-  // release but we leave it here in case someone's app does not utilize the V2 embedding yet.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val inst = FlutterSmsPlugin()
-      inst.activity = registrar.activity()
-      inst.setupCallbackChannels(registrar.messenger())
-    }
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
